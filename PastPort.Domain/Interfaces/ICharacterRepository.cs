476701f7b3a4ed2b09d86c1f0928f22fb7658f24
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2026-04-19 15:13:01 +0200

    edit character repository

--- PastPort.Domain/Interfaces/ICharacterRepository.cs
+++ PastPort.Domain/Interfaces/ICharacterRepository.cs
@@ -1,9 +1,18 @@
-﻿using PastPort.Domain.Entities;
+﻿// ✅ FIXED: ICharacterRepository.cs
+// أضفنا GetAllWithScenesAsync عشان نحل مشكلة الـ N+1 query
+// في CharacterService.GetAllCharactersAsync
+
+using PastPort.Domain.Entities;
 
 namespace PastPort.Domain.Interfaces;
 
 public interface ICharacterRepository : IRepository<Character>
 {
     Task<IEnumerable<Character>> GetCharactersBySceneIdAsync(Guid sceneId);
+
     Task<Character?> GetCharacterWithSceneAsync(Guid characterId);
+
+    // ✅ FIXED: Method جديدة بتجيب كل الـ characters مع الـ scenes بتاعتهم
+    // في query واحدة بدل N+1 queries
+    Task<List<Character>> GetAllWithScenesAsync();
 }
