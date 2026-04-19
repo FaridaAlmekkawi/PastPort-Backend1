ef6fa4b93f67166f24a6e89ce10cfd2d772e99dc
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2026-04-19 22:24:43 +0200

    edit

---
+++ PastPort.Domain/Enums/SubscriptionStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPort.Domain.Enums
+{
+    public enum SubscriptionStatus
+    {
+        PendingPayment = 0,   // Created but payment not yet confirmed
+        Active = 1,   // Payment confirmed, access granted
+        PastDue = 2,   // Renewal payment failed
+        Cancelled = 3,   // Voluntarily cancelled; may still have access until PeriodEnd
+        Expired = 4,   // PeriodEnd passed and not renewed
+        Trialing = 5,   // Free trial period
+        Paused = 6    // Admin/user paused
+    }
+
+}
