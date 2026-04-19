bf70789164bd572a210ae5ed7a80601260763393
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2026-04-19 22:28:08 +0200

    add BillingCycle

---
+++ PastPort.Domain/Enums/BillingCycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPort.Domain.Enums
+{
+    public enum BillingCycle
+    {
+        Monthly = 1,
+        Yearly = 2,
+        Weekly = 3,
+        OneTime = 4    // Lifetime / one-time purchase
+    }
+}
