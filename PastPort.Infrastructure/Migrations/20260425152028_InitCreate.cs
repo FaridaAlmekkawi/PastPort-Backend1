40aa21071ff2fc04f775435a4ae56644f851f730
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2026-04-25 20:52:26 +0300

    add new migration

---
+++ PastPort.Infrastructure/Migrations/20260425152028_InitCreate.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PastPort.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conversations_UserId",
+                table: "Conversations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PaymentTransactions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserSubscriptionId1",
+                table: "PaymentTransactions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentTransactions_UserId",
+                table: "PaymentTransactions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentTransactions_UserSubscriptionId1",
+                table: "PaymentTransactions",
+                column: "UserSubscriptionId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_CharacterId",
+                table: "Conversations",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_UserId_CharacterId",
+                table: "Conversations",
+                columns: new[] { "UserId", "CharacterId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conversations_Characters_CharacterId",
+                table: "Conversations",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentTransactions_AspNetUsers_UserId",
+                table: "PaymentTransactions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId",
+                table: "PaymentTransactions",
+                column: "UserSubscriptionId",
+                principalTable: "UserSubscriptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId1",
+                table: "PaymentTransactions",
+                column: "UserSubscriptionId1",
+                principalTable: "UserSubscriptions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserSubscriptions_AspNetUsers_UserId",
+                table: "UserSubscriptions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conversations_Characters_CharacterId",
+                table: "Conversations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentTransactions_AspNetUsers_UserId",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId1",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserSubscriptions_AspNetUsers_UserId",
+                table: "UserSubscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PaymentTransactions_UserId",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PaymentTransactions_UserSubscriptionId1",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conversations_CharacterId",
+                table: "Conversations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conversations_UserId_CharacterId",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "UserSubscriptionId1",
+                table: "PaymentTransactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PaymentTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_UserId",
+                table: "Conversations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PaymentTransactions_UserSubscriptions_UserSubscriptionId",
+                table: "PaymentTransactions",
+                column: "UserSubscriptionId",
+                principalTable: "UserSubscriptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
