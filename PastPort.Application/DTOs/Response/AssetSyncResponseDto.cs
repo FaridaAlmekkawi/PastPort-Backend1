1bf2021d4469869b25ae64e64e3e754e427a9da8
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2026-06-02 12:47:19 +0300

    edit Dto

---
+++ PastPort.Application/DTOs/Response/AssetSyncResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using PastPort.Application.DTOs.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPort.Application.DTOs.Response
+{
+  
+        public class AssetSyncResponseDto
+        {
+            public bool Success { get; set; }
+            public int TotalAssets { get; set; }
+            public List<AssetDto> Assets { get; set; } = new();
+            public string? Message { get; set; }
+        }
+   
+}
+
+
+
