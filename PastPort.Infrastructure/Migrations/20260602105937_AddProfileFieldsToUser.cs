48581125b1038dba845b628ae08e9f7ef9bd8c08
eJwtzLtuAyEQQNF/mZpi3/bSRikjRYldpEIDDA5edkHD4kcs/3ska9srnfsANIZyPsSJFpDgrOuN2Rm0Y6ebVlfWkhtHJN26fdc7aqsdVm0FAhLHi7fEG31sJ7Vuq9NvVBf8mofUN+VWzLk7vv99xjfy4UcPtdl/Nx/DWNcg4EXUek8EEjQhE4OAbOIroJ39IlPRwRs10V0woZWRT4IpRVEysaQZfRDXyJML8QoC6JY8U1a4glxKCM/nPz6nUP8= <faridamahmoud@std.mans.edu.eg>
2026-06-03 19:16:08 +0300

    edit

---
+++ PastPort.Infrastructure/Migrations/20260602105937_AddProfileFieldsToUser.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PastPort.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileFieldsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CameraEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LocationEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MicrophoneEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CameraEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LocationEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MicrophoneEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
